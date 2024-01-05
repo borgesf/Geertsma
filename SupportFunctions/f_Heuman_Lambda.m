ab7f073074bce442bc501e16a9cc0ab1492c3bc0
Filipe Borges <f3l5p7@yahoo.com.br>
2024-01-05 17:39:01 +0100

    Edited the function Heuman-Lambda to comply with the standard use of the incomplete elliptical integral

--- SupportFunctions/f_Heuman_Lambda.m
+++ SupportFunctions/f_Heuman_Lambda.m
@@ -1,9 +1,17 @@
 %% Auxiliaty Function for Heuman-Lambda Function
 
-function [L] = f_Heuman_Lambda(Beta,m)
-[K, E] = ellipke(m); 
-[F, EInc, ~] = elliptic12(Beta,m);
+% Reference 1: https://math.stackexchange.com/questions/628216/heuman-lambda-function-in-matlab
+% Reference 2: https://math.stackexchange.com/questions/108575/what-does-mean/108659#108659
 
-L = (2/pi)*(E*F + K*EInc - K*F);
+function [L] = f_Heuman_Lambda(Beta, m)
+
+m_dash = (1-m);
+
+[K,E] = ellipke(m);
+
+[incF, incE, ~] = elliptic12(Beta,m_dash);
+
+L = 2/pi * (E*incF + K*incE - K*incF );
+
 
 end
