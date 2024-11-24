21675b1d6d6c4a82b90dc5ee036823bbf716f0d4
Filipe Borges <f3l5p7@yahoo.com.br>
2024-11-24 16:43:56 +0100

    Added functionality to display quiver plots.

--- Geertsma_Example.m
+++ Geertsma_Example.m
@@ -12,11 +12,11 @@ clear;
 clc;
 
 %% Reservoir Parameters
-D = 400; % Reservoir Depth [m]
-R = 300; % Reservoir Radius [m]
-h = 100; % Reservoir Thickness [m]
+D = 500; % Reservoir Depth [m]
+R = 500; % Reservoir Radius [m]
+h = 10; % Reservoir Thickness [m]
 deltaP = -10e6; % Change in Pore Pressure [Pa]
-numLayers = 10; % Number of layers in the reservoir
+numLayers = 1; % Number of layers in the reservoir
 
 %% Medium Properties
 E = 2e9; % Young's Modulus [Pa]
@@ -25,7 +25,7 @@ kMineral = 37e9; % Bulk Modulus of Mineral [Pa]
 
 %% Output Coordinates
 zCoords = linspace(eps, 1000, 101); % Vertical positions [m]
-rCoords = linspace(eps, 1000, 101); % Radial positions [m]
+rCoords = linspace(eps, 1500, 101); % Radial positions [m]
 
 %% Function Evaluation
 % Use either symbolic or non-symbolic version based on availability
@@ -52,6 +52,7 @@ sigmaRInterp = interpolateField(sigmaRSymmetric, rGrid, zGrid, rPlot, zPlot);
 
 %% Visualization
 visualizeResults(rPlot, zPlot, uzInterp, urInterp, sigmaZInterp, sigmaRInterp, R, D, h);
+visualizeResultsQuiver(rPlot, zPlot, uzInterp, urInterp, R, D, h); % Call quiver visualization
 
 %% Helper Functions
 
@@ -142,13 +143,15 @@ title('Radial Displacement', 'FontSize', 14, 'Interpreter', 'latex');
 
 % Plot vertical stress change
 subplot(2, 2, 3);
-plotField(rPlot, zPlot, sigmaZ/1e6, 'Vertical Stress Change ($\Delta\sigma_V$) [MPa]', [], R, D, h);
+plotField(rPlot, zPlot, -sigmaZ/1e6, 'Vertical Stress Change ($\Delta\sigma_V$) [MPa]', [], R, D, h);
 title('Vertical Stress Change', 'FontSize', 14, 'Interpreter', 'latex');
 
 % Plot radial stress change
 subplot(2, 2, 4);
-plotField(rPlot, zPlot, sigmaR/1e6, 'Radial Stress Change ($\Delta\sigma_r$) [MPa]', [], R, D, h);
+plotField(rPlot, zPlot, -sigmaR/1e6, 'Radial Stress Change ($\Delta\sigma_r$) [MPa]', [], R, D, h);
 title('Radial Stress Change', 'FontSize', 14, 'Interpreter', 'latex');
+
+%exportgraphics(gcf, 'Geertsma_2.png', 'Resolution', 400);
 end
 
 function plotField(x, y, field, colorbarLabel, cLimits, R, D, h)
@@ -195,9 +198,62 @@ set(gca, 'YDir', 'reverse', 'FontSize', 12, 'Box', 'on');
 rectangle('Position', [-R, (D - h / 2), 2 * R, h], 'FaceColor', 'k'); % Reservoir rectangle
 
 % Add axis labels
-xlabel('Horizontal Distance (m)', 'Interpreter', 'latex');
+xlabel('Horizontal Coordinate (m)', 'Interpreter', 'latex');
 ylabel('Depth (m)', 'Interpreter', 'latex'); % Subsea true vertical depth
+ylim([0 max(y(:))])
 
 % Release hold
 hold off;
+end
+
+function visualizeResultsQuiver(rPlot, zPlot, uz, ur, R, D, h)
+% VISUALIZERESULTSQUIVER Visualizes displacement and stress fields using quiver plots.
+%
+% This function generates 2x1 quiver plots for:
+%   1. Displacement field (horizontal and vertical displacements)
+%   2. Stress change field (horizontal and vertical stress components)
+%
+% Inputs:
+%   rPlot   - (n, m) double array of radial coordinates for plotting [m]
+%   zPlot   - (n, m) double array of vertical coordinates for plotting [m]
+%   uz      - (n, m) double array of vertical displacements [cm]
+%   ur      - (n, m) double array of radial displacements [cm]
+%   R       - (1, 1) double, reservoir radius [m]
+%   D       - (1, 1) double, reservoir depth [m]
+%   h       - (1, 1) double, reservoir thickness [m]
+
+    % Subsampling for quiver plot readability
+    decimationFactor = 50; % Reduce density of arrows
+    rSub = rPlot(1:decimationFactor:end, 1:decimationFactor:end);
+    zSub = zPlot(1:decimationFactor:end, 1:decimationFactor:end);
+    urSub = ur(1:decimationFactor:end, 1:decimationFactor:end);
+    uzSub = uz(1:decimationFactor:end, 1:decimationFactor:end);
+   
+
+    % Adjust displacements and stresses to zero inside the reservoir
+    insideReservoir = (zSub >= D - h / 2) & (zSub <= D + h / 2);
+    uzSub(insideReservoir) = 0;
+    urSub(insideReservoir) = 0;
+
+    % Adjust ur for horizontal displacements
+    urSub(rSub < 0) = -urSub(rSub < 0);
+
+    % Create figure
+    figure('Position', [400, 100, 700, 500]);
+
+    % Plot displacement quiver plot
+    quiver(rSub, zSub, urSub, uzSub, 'r', 'LineWidth', 1.5, 'AutoScaleFactor', 0.8);
+    hold on;
+    grid on;
+    box on;
+    rectangle('Position', [-R, D - h / 2, 2 * R, h], 'FaceColor', [0.5, 0.5, 0.5, 0.5]); % Reservoir rectangle
+    set(gca, 'YDir', 'reverse');
+    xlabel('Horizontal Coordinate (m)');
+    ylabel('Depth (m)');
+    title('Displacement Field', 'Interpreter', 'latex', 'FontSize', 14);
+    xlim(max(rPlot(:))*[-1 1])
+    ylim([0 max(zPlot(:))])
+    %exportgraphics(gcf, 'Geertsma_quiver.png', 'Resolution', 400); 
+    hold off
+ 
 end
